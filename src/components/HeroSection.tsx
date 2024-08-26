80b020b60aeefbaed1369b46d0432c27315776e1
Jason Summers <jwsummers.dev@gmail.com>
2024-08-25 20:36:25 -0400

    Fix mobile responsive image

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -14,7 +14,7 @@ export default function HeroSection() {
           Welcome to <span className="text-blue-500">CodeWrench Studios</span>
         </h1>
         <p className="mt-4 text-lg text-gray-100">
-          At <span className="text-blue-500">CodeWrench Studios</span>, I are passionate about delivering impactful digital solutions that drive results and exceed expectations.
+          At <span className="text-blue-500">CodeWrench Studios</span>, I am passionate about delivering impactful digital solutions that drive results and exceed expectations.
         </p>
         <p className="mt-2 text-md text-gray-300">
           With a unique blend of hands-on precision and craftsmanship, I bring the same dedication and meticulous attention to detail to every web development project. Let me help you turn your vision into reality with cutting-edge technology and innovative design.
